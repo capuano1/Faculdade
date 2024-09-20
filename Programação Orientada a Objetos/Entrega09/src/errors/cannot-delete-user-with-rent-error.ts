e0989280a0eb78aa833b271dbf7a5172232ac32e
Caputino <10758372+capuano1@users.noreply.github.com>
2024-09-20 04:53:53 -0300

    Update POO

--- Programação Orientada a Objetos/Entrega09/src/errors/cannot-delete-user-with-rent-error.ts
+++
@@ -1,7 +0,0 @@
-export class CannotDeleteUserWithRentError extends Error {
-    public readonly name = 'CannotDeleteUserWithRentError'
-
-    constructor() {
-        super('Cannot delete user with an active rent.')
-    }
-}
--- Programação Orientada a Objetos/Semana 22-11/bike-rent-poo-master/src/errors/user-has-open-rent-error.ts
+++
@@ -1,7 +0,0 @@
-export class UserHasOpenRentError extends Error {
-    public readonly name = 'UserHasOpenRentError'
-
-    constructor() {
-        super('User has open rent.')
-    }
-}
